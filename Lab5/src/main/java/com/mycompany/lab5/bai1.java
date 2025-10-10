12b2ec102055984ad2abf44a357c6e27a7b9860c
Admin <Admin@Tienthom>
2025-10-10 11:53:07 +0000

    nop lab5

--- Lab5/src/main/java/com/mycompany/lab5/bai1.java
+++ Lab5/src/main/java/com/mycompany/lab5/bai1.java
@@ -3,6 +3,8 @@
  */
 
 package com.mycompany.lab5;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +13,32 @@ package com.mycompany.lab5;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> list = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Nhap so thuc: ");
+            double x = sc.nextDouble();
+            list.add(x);
+
+            System.out.print("ban co muon nhap them khong? (Y/N): ");
+            sc.nextLine(); 
+            String tiepTuc = sc.nextLine();
+            if (tiepTuc.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+
+        System.out.println("\nDanh sach cac so thuc ban da nhap:");
+        for (double num : list) {
+            System.out.print(num + "  ");
+        }
+
+        double tong = 0;
+        for (double num : list) {
+            tong += num;
+        }
+
+        System.out.println("\nTong cac so: " + tong);
     }
 }
